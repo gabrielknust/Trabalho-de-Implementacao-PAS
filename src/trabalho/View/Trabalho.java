4e352cbdfbfc3ee37a00167dd87652c06f6f6821
FabricioLourran <fabriciolourran0007@gmail.com>
2023-12-13 23:28:51 -0300

    main

--- src/trabalho/View/Trabalho.java
+++ src/trabalho/View/Trabalho.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package trabalho.View;
+import trabalho.Control;
 
 /**
  *
@@ -14,7 +15,74 @@ public class Trabalho {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+           while (true) {
+            System.out.println("Escolha uma opção:");
+            System.out.println("1 - Cadastrar Cliente");
+            System.out.println("2 - Cadastrar Corretor");
+            System.out.println("3 - Cadastrar Imóvel");
+            System.out.println("4 - Agendar visita");
+            System.out.println("5 - Imprimir Agenda");        
+            System.out.println("6 - mudar a taxa de visita");
+            System.out.println("7 - Vender ou Alugar");
+            System.out.println("8 - listar clientes");
+            System.out.println("9 - listar imoveis");
+            System.out.println("10 - listar corretores");
+            System.out.println("11 - relatorio mensal");
+            System.out.println("12 - relatorio anual");            
+            System.out.println("13 - Sair");
+
+            int opcao = scanner.nextInt();
+        
+            switch (opcao) {
+                case 1:
+                   CompradorControl.create();
+                    break;
+                case 2:
+                    CorretorControl.create();
+                    break;
+                case 3:
+                    ImovelControl.create();
+                    break;
+                case 4:
+                    VisitaControl.create();
+                    break;
+                case 5:
+                    VisitaControl.readAll();
+                    break;
+                case 6:
+                    System.out.println("Digite a nova taxa");
+                    taxa = scanner.nextFloat();
+                    VisitaControl.atualizaTaxa(taxa);             
+                    break;
+                    
+                case 7:
+                    TransacaoControl.create();
+                    break;
+                case 8:
+                    CompradorControl.readAll();
+                    break;
+                case 9:
+                    ImovelControl.readAll();
+                    break;    
+                case 10:
+                    CorretorControl.readAll();
+                    break;
+                case 11:
+                    RelatorioControl.mensal();
+                    break;
+                case 12:
+                    RelatorioControl.anual();
+                    break;
+               
+                case 13:
+                    System.out.println("Encerrando o programa.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
     }
     
 }
